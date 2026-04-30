169f84c33b4c77229ba7d70cfc5c2bff6d91a490
Nurlan <suleymanlinurlan85@gmail.com>
2026-05-01 01:58:56 +0400

    create customer-service microservice

---
+++ customer-service/src/test/java/com/nurlansuleymanli/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nurlansuleymanli.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
