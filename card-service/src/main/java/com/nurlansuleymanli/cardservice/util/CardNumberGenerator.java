fee1168bde374864a14e49eff42c5d33e57c05b0
Nurlan <suleymanlinurlan85@gmail.com>
2026-05-18 18:42:13 +0400

    added helper class for card number generate

---
+++ card-service/src/main/java/com/nurlansuleymanli/cardservice/util/CardNumberGenerator.java
@@ -0,0 +1,24 @@
+package com.nurlansuleymanli.cardservice.util;
+
+import java.util.Random;
+
+public class CardNumberGenerator {
+
+    private static final String number= "54117388";
+    private static final Random random= new Random();
+
+
+    public static String generate(){
+        StringBuilder cardNumber= new StringBuilder(number);
+
+        for (int i = 0; i < 8; i++) {
+            cardNumber.append(random.nextInt(10));
+        }
+
+        return cardNumber.toString();
+    }
+
+
+
+
+}
