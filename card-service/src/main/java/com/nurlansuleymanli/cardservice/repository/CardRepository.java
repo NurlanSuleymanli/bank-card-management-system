0b40178aaaff9fafe539fafeba9780bbfc0b7871
Nurlan <suleymanlinurlan85@gmail.com>
2026-05-21 13:20:42 +0400

    added service and repository for card

---
+++ card-service/src/main/java/com/nurlansuleymanli/cardservice/repository/CardRepository.java
@@ -0,0 +1,4 @@
+package com.nurlansuleymanli.cardservice.repository;
+
+public interface CardRepository {
+}
---
+++ card-service/src/main/java/com/nurlansuleymanli/cardservice/service/CardService.java
@@ -0,0 +1,4 @@
+package com.nurlansuleymanli.cardservice.service;
+
+public class CardService {
+}
