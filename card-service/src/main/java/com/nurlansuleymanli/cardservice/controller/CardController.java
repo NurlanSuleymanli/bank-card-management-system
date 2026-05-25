533c8e084ea3acec59fab3fa03eeb01290051965
Nurlan <suleymanlinurlan85@gmail.com>
2026-05-25 14:34:24 +0400

    added get card endpoint

--- card-service/src/main/java/com/nurlansuleymanli/cardservice/controller/CardController.java
+++ card-service/src/main/java/com/nurlansuleymanli/cardservice/controller/CardController.java
@@ -8,10 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/cards")
@@ -25,4 +22,12 @@ public class CardController {
     public ResponseEntity<CreateCardResponse> createCard(@RequestBody CreateCardRequest request){
         return ResponseEntity.status(HttpStatus.OK).body(cardService.createCard(request));
     }
+
+    //GET /api/v1/cards/{cardId} — Kart məlumatlarını qaytarır
+
+    @GetMapping("/{cardId}")
+    public ResponseEntity<CardResponse> getCard(@PathVariable Long cardId){
+        return ResponseEntity.ok(cardService.getCard(cardId));
+    }
+
 }
