c8519566bb2e26394dfa2d846f5e06914bb6aa7d
Nurlan <suleymanlinurlan85@gmail.com>
2026-08-23 17:21:07 +0400

    small updates

--- card-service/src/main/java/com/nurlansuleymanli/cardservice/config/SecurityConfig.java
+++ card-service/src/main/java/com/nurlansuleymanli/cardservice/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.nurlansuleymanli.cardservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,6 +21,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
                 );
